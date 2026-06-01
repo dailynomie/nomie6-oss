98ad2b89c1a04111073d4e77eff0bc59c556a14d
ronald de lange <ronald_de_lange@hotmail.com>
2026-06-02 00:09:55 +0200

    feat: Migrate PivotTable to Svelte 5 runes mode
    Convert getConfig reactive statement to $effect():
    - Builds PivotClass config when getConfig signal changes
    - Uses $props() and restProps for all prop access
    - Properly spreads remaining props to child renderer component

--- src/domains/analytics/Pivot-table/PivotTable.svelte
+++ src/domains/analytics/Pivot-table/PivotTable.svelte
@@ -1,58 +1,66 @@
+<svelte:options runes={true} />
+
 <script lang="ts">
     import TableRenderers from './TableRenderers';
     import { PivotClass } from '../pivot-class';
     import { aggregators as defaultAggregators } from '../Pivot-table/Utilities';
 
-    export let renderer = TableRenderers.Table;
-    export let pivotconfig;
-    export let getConfig = false;
-    export let workingPivotId = "1dummy2";
-    export let workingPivotTag = "Dummy";
-    export let workingPivotEmoji = "🐣";
-    export let workingPivotDefault = false;
-    export let workingPivotDays = 90;
-    export let workingPivotSearchTerm = {"enabled":false,terms:""}
-    export let renderers;
-    
-    let aggregators = defaultAggregators;
+    const {
+        renderer = TableRenderers.Table,
+        pivotconfig: pivotconfigProp = undefined,
+        getConfig = false,
+        workingPivotId = "1dummy2",
+        workingPivotTag = "Dummy",
+        workingPivotEmoji = "🐣",
+        workingPivotDefault = false,
+        workingPivotDays = 90,
+        workingPivotSearchTerm = {"enabled":false,terms:""},
+        renderers = {},
+        ...restProps
+    } = $props();
 
-    $: if(getConfig){
-        let config = $$restProps;
-        config.renderer = renderer;
-        let currentconfig = new PivotClass({
-      id: workingPivotId,
-      tag: workingPivotTag,
-      emoji: workingPivotEmoji,
-      default: workingPivotDefault,
-      days: workingPivotDays,
-      searchterm: workingPivotSearchTerm,
-      grouping: config.grouping,
-      compactRows: config.compactRows,
-      rowGroupBefore: config.rowGroupBefore,
-      colGroupBefore: config.colGroupBefore,
-      rendererName: Object.keys(renderers).find(key => renderers[key] === renderer) || "Table",
-      aggregatorName: Object.keys(aggregators).find(key => aggregators[key] === config.aggregator) || "Count",
-      hiddenAttributes: config.hiddenAttributes || [],
-        hiddenFromAggregators: config.hiddenFromAggregators || [],
-        hiddenFromDragDrop: config.hiddenFromDragDrop || [],
-        unusedOrientationCutoff: config.unusedOrientationCutoff || 85,
-        menuLimit: config.menuLimit || 500,
-        options: {
-        "rows": config.rows || [],
-        "cols": config.cols || [],
-        "vals": config.vals || [],
-        "valueFilter": config.valueFilter || {},
-        "sorters": config.sorters || {},
-        "derivedAttributes":config.derivedAttributes,
-    },})
-    pivotconfig = currentconfig;
-    }    
+    let pivotconfig = $state(pivotconfigProp);
+    let aggregators = $state(defaultAggregators);
 
+    // Build pivot config when getConfig changes
+    $effect(() => {
+        if (getConfig) {
+            const config = { ...restProps, renderer };
+            const currentconfig = new PivotClass({
+                id: workingPivotId,
+                tag: workingPivotTag,
+                emoji: workingPivotEmoji,
+                default: workingPivotDefault,
+                days: workingPivotDays,
+                searchterm: workingPivotSearchTerm,
+                grouping: config.grouping,
+                compactRows: config.compactRows,
+                rowGroupBefore: config.rowGroupBefore,
+                colGroupBefore: config.colGroupBefore,
+                rendererName: Object.keys(renderers).find(key => renderers[key] === renderer) || "Table",
+                aggregatorName: Object.keys(aggregators).find(key => aggregators[key] === config.aggregator) || "Count",
+                hiddenAttributes: config.hiddenAttributes || [],
+                hiddenFromAggregators: config.hiddenFromAggregators || [],
+                hiddenFromDragDrop: config.hiddenFromDragDrop || [],
+                unusedOrientationCutoff: config.unusedOrientationCutoff || 85,
+                menuLimit: config.menuLimit || 500,
+                options: {
+                    "rows": config.rows || [],
+                    "cols": config.cols || [],
+                    "vals": config.vals || [],
+                    "valueFilter": config.valueFilter || {},
+                    "sorters": config.sorters || {},
+                    "derivedAttributes": config.derivedAttributes,
+                },
+            });
+            pivotconfig = currentconfig;
+        }
+    });
 </script>
 
 
 <div class="cont">
-<svelte:component this={renderer} {pivotconfig} {...$$restProps} />
+<svelte:component this={renderer} {pivotconfig} {...restProps} />
 </div>
 
 <style>
